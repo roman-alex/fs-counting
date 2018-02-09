66a91211a8554abd318148cb1ddd41adc152d79e
Roman Alekseev <roman.alekseev555@gmail.com>
2018-02-09 15:18:46 +0200

    filter by time

--- src/pages/retail-list/retail-list.ts
+++ src/pages/retail-list/retail-list.ts
@@ -5,6 +5,9 @@ import {Observable} from "rxjs/Observable";
 import {Retail} from "../../models/retail";
 import {AngularFireAuth} from "angularfire2/auth";
 import * as firebase from "firebase/app";
+import {BehaviorSubject} from "rxjs/BehaviorSubject";
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/observable/combineLatest';
 
 @IonicPage()
 @Component({
@@ -15,18 +18,21 @@ export class RetailListPage {
 
   private retailCollection: AngularFirestoreCollection<Retail>;
   retails: Observable<any[]>;
-  countItems = 0;
+  countItems: number;
+  income: number;
+  retail: number;
+  wholesale: number;
+  surcharge: number;
+  fromDate: string;
+  toDate: string;
+  dateFilter$ = new BehaviorSubject(null);
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private afs: AngularFirestore, private afAuth: AngularFireAuth) {
     this.afAuth.authState.subscribe((user: firebase.User) => {
       if (user) {
-        this.retailCollection = this.afs.doc<any>(`users/${user.uid}`).collection<Retail>('retails', ref => {
-          return ref.orderBy('time')
-        });
-        this.retails = this.retailCollection.snapshotChanges().map(actions => {
-          this.countItems = actions.length;
-          return actions.map(action => ({ $key: action.payload.doc.id, ...action.payload.doc.data() }));
-        });;
+        this.fromDate = this.tomonth();
+        this.toDate = this.today();
+        this.getRetails(user.uid);
       }
     });
   }
@@ -46,6 +52,59 @@ export class RetailListPage {
 
   deleteItem(key) {
     this.retailCollection.doc(key).delete();
+  }
+
+  getRetails(uid) {
+    this.retails = Observable.combineLatest(
+      this.dateFilter$
+    ).switchMap(() =>
+      this.afs.doc<any>(`users/${uid}`).collection<Retail>('retails', ref => {
+        return ref.where('time', '<=', this.toDate).where('time', '>=', this.fromDate).orderBy('time', 'desc');
+      }).snapshotChanges().map(actions => {
+        this.countItems = actions.length;
+        this.income = 0;
+        this.retail = 0;
+        this.wholesale = 0;
+        this.surcharge = 0;
+        actions.map(action => {
+          this.income = this.income + (action.payload.doc.data().retail - action.payload.doc.data().wholesale - action.payload.doc.data().surcharge);
+          this.retail = this.retail + (+action.payload.doc.data().retail);
+          this.wholesale = this.wholesale + (+action.payload.doc.data().wholesale);
+          this.surcharge = this.surcharge + (+action.payload.doc.data().surcharge);
+        });
+        return actions.map(action => ({ $key: action.payload.doc.id, ...action.payload.doc.data() }));
+      })
+    );
+  }
+
+  searchByDate() {
+    this.dateFilter$.next(null);
+  }
+
+  today() {
+    let today: any = new Date();
+    let dd: any = today.getDate();
+    let mm: any = today.getMonth()+1;
+    let yyyy: any = today.getFullYear();
+    if(dd<10){
+      dd='0'+dd;
+    }
+    if(mm<10){
+      mm='0'+mm;
+    }
+    today = yyyy+'-'+mm+'-'+dd;
+    return today;
+  }
+
+  tomonth() {
+    let today: any = new Date();
+    let mm: any = today.getMonth()+1;
+    let yyyy: any = today.getFullYear();
+    if(mm<10){
+      mm='0'+mm;
+    }
+    today = yyyy+'-'+mm+'-'+'01';
+    return today;
   }
 
 }
